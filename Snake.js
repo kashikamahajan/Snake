1df913bd93782f7a1b9654c57efc9ef6b3cbd5f7
kashikamahajan <61978407+kashikamahajan@users.noreply.github.com>
2020-04-05 16:37:36 +0530

    Add files via upload

---
+++ Snake.js
@@ -0,0 +1,75 @@
+class Snake {
+  
+  constructor() {
+  	this.body = [];
+    this.body[0] = createVector(floor(w/2), floor(h/2));
+    this.xp = 0;
+    this.yp = 0;
+    this.len=0;
+    
+  }
+  
+  setDir(x, y) {
+  	this.xp = x;
+    this.yp = y;
+  }
+  
+  update() {
+    var head = this.body[this.body.length-1].copy();
+    
+    this.body.shift();
+    head.x += this.xp;
+    head.y += this.yp;
+    this.body.push(head);
+  }
+  
+  grow() {
+  	var head = this.body[this.body.length-1].copy();
+    this.len++
+    this.body.push(head);
+  }
+  
+  endGame() {
+  	var x = this.body[this.body.length-1].x;
+    var y = this.body[this.body.length-1].y;
+    if(x > w-1 || x < 0 || y > h-1 || y < 0) {
+       return true;
+    }
+    for(var i = 0; i < this.body.length-1; i++) {
+    	var part = this.body[i];
+      if(part.x === x && part.y === y) {
+      	return true;
+      }
+    }
+    return false;
+  }
+  
+  eat(pos) {
+  	var x = this.body[this.body.length-1].x;
+    var y = this.body[this.body.length-1].y;
+    if(x === pos.x && y === pos.y) {
+      this.grow();
+      return true;
+    }
+    return false;
+  }
+  
+  show() {
+  	for(var i = 1; i < this.body.length; i++) {
+      if(i!==this.len)
+      {
+        imageMode(CENTER);
+        image(b,this.body[i].x,this.body[i].y,1.5,1.5);
+      }
+      
+      
+      
+      //console.log(i);
+    }
+      imageMode(CENTER);
+      image(sh,this.body[this.len].x,this.body[this.len].y,1.5,1.5);
+      
+    
+  }
+
+}
